9a7c5b29eeb5cc30920ecbc6f35c2b9ce10c7e7e
Dominykas Vozgirdas <dominykas.vozgirdas@ku.lt>
2024-06-10 18:06:11 +0300

    Some Changes

--- myrpggame/MainWindow.xaml.cs
+++ myrpggame/MainWindow.xaml.cs
@@ -26,5 +26,10 @@ namespace myrpggame
             DataContext = _gameSession;
 
         }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            _gameSession.CurrentPlayer.ExperiencePoints = _gameSession.CurrentPlayer.ExperiencePoints + 10;
+        }
     }
 }

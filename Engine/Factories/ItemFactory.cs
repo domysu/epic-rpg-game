25587ec25fc33f56c5ef751b6530731bdd93f5ff
Dominykas Vozgirdas <dominykas.vozgirdas@ku.lt>
2024-06-12 00:15:11 +0300

    Add item, weapon creation logic

---
+++ Engine/Factories/ItemFactory.cs
@@ -0,0 +1,34 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Factories
+{
+    public static class ItemFactory
+    {
+        public static List<GameItem> _standardGameItems;
+
+
+        static ItemFactory()
+        {
+
+            _standardGameItems = new List<GameItem>();
+            _standardGameItems.Add(new Weapons(1001, "Wooden Stick",100, 1, 2));
+            _standardGameItems.Add(new Weapons(1002, "Wooden Sword", 250, 2, 4));
+        }
+        public static GameItem CreateGameItem(int itemTypeID)
+        {
+            GameItem standardItem = _standardGameItems.FirstOrDefault(item => item.ItemTypeID == itemTypeID);
+
+            if(standardItem != null) {
+            return standardItem.Clone();
+            
+            }
+            return null;
+
+        }
+    }
+}
---
+++ Engine/Models/Weapons.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    internal class Weapons : GameItem
+    {
+
+        public int MinimumDamage {  get; set; }
+        public int MaximumDamage { get; set; }
+        public Weapons(int itemTypeID, string name, int price, int minimumDamage, int maximumDamage) : base(itemTypeID, name, price)
+
+        {
+            MinimumDamage = minimumDamage;
+            MaximumDamage = maximumDamage;
+        }
+
+     
+
+        public new Weapons Clone()
+        {
+            return new Weapons(ItemTypeID, Name, Price,MinimumDamage, MaximumDamage);
+        }
+    }
+}

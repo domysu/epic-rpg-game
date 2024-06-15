c42128dff1c0cede2baf64af98e01f3ee37d2588
Dominykas Vozgirdas <dominykas.vozgirdas@ku.lt>
2024-06-15 18:38:43 +0300

    Add Monsters

---
+++ Engine/Factories/MonsterFactory.cs
@@ -0,0 +1,57 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Factories
+{
+    internal static class MonsterFactory
+    {
+        private static readonly List<Monster> _monsters = new List<Monster>();
+
+
+        public static Monster GetMonster(int MonsterID)
+        {
+
+            switch(MonsterID)
+            {
+                case 1:
+                    {
+                        Monster snake = new Monster(2, "Snake", "A venomous snake", "Snake.png", 20, 25);
+                        AddLootItem(snake, 9001, 75);
+                        AddLootItem(snake, 9002, 25);
+                        return snake;
+                     
+                    }
+                case 2:
+                    {
+                        Monster giantSpider = new Monster(4, "GiantSpider", "A dangerous spider", "GiantSpider.png", 25, 35);
+                        AddLootItem(giantSpider, 9005, 25);
+                        AddLootItem(giantSpider, 9006, 75);
+                        return giantSpider;
+
+                    }
+                case 3:
+                    {
+                        Monster rat = new Monster(1, "Rat", "Cheesy Rat", "Rat.png", 10, 10);
+                        AddLootItem(rat, 9003, 25);
+                        AddLootItem(rat, 9004, 75);
+                        return rat;
+                    }
+                default:
+                    throw new ArgumentException(string.Format("No spider with id '{0}' exists", MonsterID));
+            }
+        }
+        private static void AddLootItem(Monster monster, int itemID, int percentage)
+        {
+            if (RandomNumberGenerator.NumberBetween(1, 100) <= percentage)
+            {
+                monster.Inventory.Add(new ItemQuantity(itemID, 1));
+            }
+        }
+    
+
+    }
+}
---
+++ Engine/Models/Monster.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Monster : BaseNotificationClass
+    {
+
+        public int ID;
+        public int _hitpoints;
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageName { get; set; }
+
+        public int HitPoints {
+            get
+            {
+                return _hitpoints;
+            }
+
+            set
+            {
+                _hitpoints = value;
+                OnPropertyChanged(nameof(HitPoints));
+            }
+
+        }
+        public int RewardExperiencePoints { get; set; }
+        public int RewardGold { get;set; }
+
+        public ObservableCollection<ItemQuantity> Inventory { get; set; }
+
+
+        public Monster(int hitPoints, string name, string description, string imageName, int rewardExperiencePoints, int rewardGold)
+        {
+
+            HitPoints = hitPoints;
+            Name = name;
+            Description = description;
+            ImageName = string.Format("pack://application:,,,/Engine;component/Images/Monsters/{0}", imageName);
+            RewardExperiencePoints = rewardExperiencePoints;
+            RewardGold = rewardGold;
+
+
+        }
+
+    }
+}

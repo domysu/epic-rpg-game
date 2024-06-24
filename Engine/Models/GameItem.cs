6bca58bfff36f29f2f6195b8c837006f1e7cb60d
Dominykas Vozgirdas <dominykas.vozgirdas@ku.lt>
2024-06-24 22:32:34 +0300

    Some changes

--- Engine/Models/GameItem.cs
+++ Engine/Models/GameItem.cs
@@ -11,16 +11,17 @@ namespace Engine.Models
         public int ItemTypeID { get; set; }
         public string Name { get; set; }
         public int Price {  get; set; }
-
-        public GameItem(int itemTypeID, string name, int price) {
+        public bool IsUnique { get; set; }
+        public GameItem(int itemTypeID, string name, int price, bool isUnique = false) {
 
             ItemTypeID = itemTypeID;
             Name = name;    
             Price = price;
+            IsUnique = isUnique;
         
         }
         public GameItem Clone() {
-        return new(ItemTypeID, Name, Price);    
+        return new(ItemTypeID, Name, Price, IsUnique);    
         
         }
     }
